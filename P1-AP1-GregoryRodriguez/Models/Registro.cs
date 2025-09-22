5b614dfcc21a3eca35c5e2486c2daba11d34327b
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2025-09-22 19:25:03 -0400

    Se creo el modelo "Registro" y se modifico el nombre del proyecto.

---
+++ P1-AP1-GregoryRodriguez/Models/Registro.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_GregoryRodriguez.Models;
+public class Registro
+{
+    [Key]
+    int Id { get; set; }
+    string Nombres { get; set; }
+}
+
