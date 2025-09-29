4a604c29cf97ed8217f4df3970ad4050721fc209
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2025-09-29 18:27:10 -0400

    Se crearon los campos, IdEntrada, NombreCliente,Fecha,Cantidad y precio para el modelo "EntradaGuacales"

--- P1-AP1-GregoryRodriguez/Models/EntradasGuacales.cs
+++ P1-AP1-GregoryRodriguez/Models/EntradasGuacales.cs
@@ -4,7 +4,16 @@ namespace P1_AP1_GregoryRodriguez.Models;
 public class EntradasGuacales
 {
     [Key]
-    public int Id { get; set; }
-    public string Nombres { get; set; }
+    public int IdEntrada { get; set; }
+
+    [Required(ErrorMessage = "El nombre es un campo obligatorio")]
+    public string NombreCliente { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Range(0, int.MaxValue, ErrorMessage = "La cantidad no puede ser menor a cero")]
+    public int Cantidad { get; set; }
+
+    [Range(0.0, double.MaxValue, ErrorMessage = "El precio no puede ser menor que cero")]
+    public double Precio { get; set; }
 }
 
